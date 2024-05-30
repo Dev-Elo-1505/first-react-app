c4b2cba002a47d32c6cb34878eef0d53220e6e46
Joy Addisi <elooghenejoy@gmail.com>
2024-05-30 10:09:55 +0100

    Use of props and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import ListGroup from "./components/ListGRoup";
 
 function App() {
-  return <div><ListGroup /></div>
+  let items = [
+    "Biotechnology",
+    "Computer Science",
+    "Accounting",
+    "Microbiology",
+    "Architecture",
+  ];
+  return <div><ListGroup items={items} heading='Programs' /></div>
 }
 
 export default App;
