e18d15ccd23490fe96e8b095e5209114d01facfd
Joy Addisi <elooghenejoy@gmail.com>
2024-06-16 17:04:08 +0100

    Validate form with zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { ExpenseList } from "./expense-tracker/components/ExpenseList";
 import { ExpenseFilter } from "./expense-tracker/components/ExpenseFilter";
-import { ExpenseForm } from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+
+
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
